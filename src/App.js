140e6f1143ec92daeefe53b4e895f063509bc862
matiaschanquia <matiaschanquianahuel@gmail.com>
2022-12-23 00:26:23 -0300

    navigate home

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { useState } from "react";
 import Cards from "./components/Cards.jsx";
 import Nav from "./components/Nav";
 import "./global.css";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import About from "./components/About.jsx";
 import Footer from "./components/Footer.jsx";
 import Detail from "./components/Detail.jsx";
 
 function App() {
     const [characters, setCharacters] = useState([]);
-
+    const navigate = Navigate();
+    navigate("/home");
     const onSearch = (character) => {
         fetch(`https://rickandmortyapi.com/api/character/${character}`)
             .then((response) => response.json())
